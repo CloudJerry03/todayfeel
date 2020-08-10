f6ae438029acb97323c8bb17981cceafcd6f0b00
easyrock122 <easyrack2@gmail.com>
2020-08-10 17:21:56 +0900

    sd

--- a.js
+++ a.js
@@ -7,29 +7,22 @@ $(function() {
         step : 1,
         orientation : 'horizontal',
         animate :'slow',
-        slide:function(event,u){
-            $('#data').val(u.value);
-        }
+        slide:function(event,ui){
+            $('#data').val(ui.value);
+            
+                if (100 >= ui.value && ui.value >= 81 ) {
+                        document.getElementById('result-box-a').value = '오늘의 기분은 매우 좋음';
+                   } else if (81 > ui.value && ui.value >= 61) {
+                        document.getElementById('result-box-a').value = '오늘의 기분은 좋음';
+                    } else if(61 > ui.value && ui.value >= 41){
+                        document.getElementById('result-box-a').value = '오늘의 기분은 보통';
+                    }else if(41 > ui.value && ui.value >= 21){
+                        document.getElementById('result-box-a').value = '오늘의 기분은 나쁨';
+                    }else {
+                        document.getElementById('result-box-a').value = '오늘의 기분은 매우 나쁨';
+                    }
+                }
+                });        
+
+            $('#data').val($('#slider-bar').slider('value'));
     });
-    $('#data').val($('#slider-bar').slider('value'));
-});
-
-
-let a = document.getElementById('data')
-const b = document.getElementById('slider-bar') 
-
-b.addEventListener("mousemove",()=> {
-    if (100 >= a.value && a.value >= 81 ) {
-            document.getElementById('result-box-a').value = '오늘의 기분은 매우 좋음';
-       } else if (81 > a.value && a.value >= 61) {
-            document.getElementById('result-box-a').value = '오늘의 기분은 좋음';
-        } else if(61 > a.value && a.value >= 41){
-            document.getElementById('result-box-a').value = '오늘의 기분은 보통';
-        }else if(41 > a.value && a.value >= 21){
-            document.getElementById('result-box-a').value = '오늘의 기분은 나쁨';
-        }else {
-            document.getElementById('result-box-a').value = '오늘의 기분은 매우 나쁨';
-        }
-        
-    }
-) 
